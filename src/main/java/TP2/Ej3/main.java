375db71c958527846fd25b8ffe106b9adb8e66be
KevinAxelDL <kevin_axel_99@hotmail.com>
2022-08-24 15:57:37 -0700

    Avance

--- src/main/java/TP2/Ej3/main.java
+++ src/main/java/TP2/Ej3/main.java
@@ -10,6 +10,12 @@ package TP2.Ej3;
  */
 public class main {
     public static void main (String [] args){
+        porExtension();
+        porInterfaz();
+    }
+    
+    public static void porExtension(){
+        //Implementacion de hilos por extension
         ThreadEjemplo1 t1, t2;
         t1 = new ThreadEjemplo1("Maria Jose");
         t2 = new ThreadEjemplo1("Jose Maria");
@@ -23,6 +29,17 @@ public class main {
         }catch(InterruptedException e){
             System.out.println(e.getMessage());
         }
-        System.out.println("Termina thread main");
+        System.out.println("FIN METODO porExtension");
+    }
+    
+    public static void porInterfaz(){
+        //Implementacion de hilos mediante interfaz
+        Thread t1, t2;
+        ThreadEjemplo2 obj1, obj2;
+        //
+        t1 = new Thread();
+        t2 = new Thread();
+        //
+        
     }
 }
